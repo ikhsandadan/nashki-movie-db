7aa33fa03ac179daf1da64031c535ec1402961f5
Nashki <116878888+ikhsandadan@users.noreply.github.com>
2024-02-22 14:23:21 +0700

    Update Slider.jsx

--- src/app/(components)/Slider.jsx
+++ src/app/(components)/Slider.jsx
@@ -26,7 +26,7 @@ const Slider = (movies) => {
         >
             {Movies?.map((movie) => {
                 return (
-                    <SwiperSlide key={movie.id} className="scale-90">
+                    <SwiperSlide key={movie.id}>
                         <Link href={`/Moviedetail/${movie.id}`} style={{textDecoration: "none", color:"white"}}>
                             <Tooltip title={movie.original_title}>
                                 <div  className="max-w-56 rounded hover:scale-105">
